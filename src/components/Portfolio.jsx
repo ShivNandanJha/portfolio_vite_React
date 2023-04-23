e364f7280a3fb88e0f8e9dced5a7842fb50b3956
Shiv Nandan Jha <neureto12@gmail.com>
2023-04-23 11:04:45 +0530

    Project Section Updated

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,79 +1,107 @@
 import React, { useState } from "react";
-
+import github from "../assets/github.png";
 import { projects, filters } from "../data";
 import Div from "./Div";
 
 const Portfolio = () => {
-    const [selectedTag, setSelectedTag] = useState("all");
-    const [projectData, setProjectData] = useState(projects);
+  const [selectedTag, setSelectedTag] = useState("all");
+  const [projectData, setProjectData] = useState(projects);
 
-    const filterHandler = (tag) => {
-        setSelectedTag(tag);
-        let filteredProjects = [];
-        if (tag === "all") {
-            filteredProjects = projects;
-        } else {
-            filteredProjects = projects.filter((p) => p.tags.includes(tag));
-        }
-        setProjectData(filteredProjects);
-    };
+  const filterHandler = (tag) => {
+    setSelectedTag(tag);
+    let filteredProjects = [];
+    if (tag === "all") {
+      filteredProjects = projects;
+    } else {
+      filteredProjects = projects.filter((p) => p.tags.includes(tag));
+    }
+    setProjectData(filteredProjects);
+  };
 
-    return (
-        <div id="work" className="mt-10 relative">
-            {/* SECTION HEADING START */}
-            <Div className="mb-10">
-                <div className="flex text-center justify-center gap-2 md:gap-0 md:flex-col text-[40px] md:text-[90px] 2xl:text-[120px] leading-[40px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase text-gradient mb-2">
-                    Latest Projects
-                </div>
-                <div className="text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] text-[#CCCCCC] text-center">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry.
-                </div>
-            </Div>
-            {/* SECTION HEADING END */}
-
-            {/* FILTER TAGS START */}
-            <Div className="flex justify-start md:justify-center gap-[10px] mb-[50px] overflow-auto">
-                {filters.map((item, index) => (
-                    <div
-                        key={index}
-                        className={`bg-[#252525] rounded-lg py-[10px] px-[17px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] cursor-pointer whitespace-nowrap ${
-                            selectedTag === item.id
-                                ? "bg-[#EFB946] text-black"
-                                : ""
-                        }`}
-                        onClick={() => filterHandler(item.id)}
-                    >
-                        {item.name}
-                    </div>
-                ))}
-            </Div>
-            {/* FILTER TAGS START */}
-
-            {/* PROJECTS GRID START */}
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-                {projectData.map((item, index) => {
-                    return (
-                        <Div
-                            key={index}
-                            className="flex flex-col gap-4 cursor-pointer"
-                        >
-                            <div className="bg-black rounded-[20px] aspect-video overflow-hidden">
-                                <img
-                                    src={item.image}
-                                    className="transition-transform hover:-translate-y-[25%] hover:duration-2500 ease-linear"
-                                />
-                            </div>
-                            <div className="text-[20px] 2xl:text-[24px] text-[#CCCCCC] text-center">
-                                {item.name}
-                            </div>
-                        </Div>
-                    );
-                })}
-            </div>
-            {/* PROJECTS GRID START */}
+  return (
+    <div id="work" className="mt-10 relative">
+      {/* SECTION HEADING START */}
+      <Div className="mb-10">
+        <div className="flex text-center justify-center gap-2 md:gap-0 md:flex-col text-[40px] md:text-[90px] 2xl:text-[120px] leading-[40px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase text-gradient mb-2">
+          Latest Projects
         </div>
-    );
+        <div className="text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] text-[#CCCCCC] text-center">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </div>
+      </Div>
+      {/* SECTION HEADING END */}
+
+      {/* FILTER TAGS START */}
+      <Div className="flex justify-start md:justify-center gap-[10px] mb-[50px] overflow-auto">
+        {filters.map((item, index) => (
+          <div
+            key={index}
+            className={`bg-[#252525] rounded-lg py-[10px] px-[17px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] cursor-pointer whitespace-nowrap ${
+              selectedTag === item.id ? "bg-[#EFB946] text-black" : ""
+            }`}
+            onClick={() => filterHandler(item.id)}
+          >
+            {item.name}
+          </div>
+        ))}
+      </Div>
+      {/* FILTER TAGS START */}
+
+      {/* PROJECTS GRID START */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        {projectData.map((item, index) => {
+          return (
+            <Div key={index} className="flex flex-col gap-4 cursor-pointer">
+              <div className="bg-black rounded-[20px] aspect-video overflow-hidden z-0">
+                <img
+                  src={item.image}
+                  className="transition-transform hover:-translate-y-[25%] hover:duration-2500 ease-linear"
+                />
+              </div>
+              {/*    github icon */}
+              <div className="absolute rounded-full bg-black flex justify-start m-3 opacity-100 ">
+                <div className = "black-gradient w-10 h-10 rounded-full flex justify-center opacity-100  items-center cursor-pointer ">
+                  <a
+                    className = "object-contain flex item-center justify-center "
+                    href      = {item.live_url}
+                    target    = "-blank"
+                  >
+                    {" "}
+                    <img
+                      src       = {github}
+                      alt       = "source code"
+                      className = "w-1/2 h-1/2 object-contain "
+                    />{" "}
+                  </a>
+                </div>
+                <div className = "black-gradient w-10 h-10 rounded-full flex justify-center opacity-100  items-center cursor-pointer ">
+                  <a
+                    className = "object-contain flex item-center justify-center "
+                    href      = {item.github_url}
+                    target    = "-blank"
+                  >
+                    {" "}
+                    <img
+                      src       = {github}
+                      alt       = "source code"
+                      className = "w-1/2 h-1/2 object-contain "
+                    />{" "}
+                  </a>
+                </div>
+              </div>
+           
+              {/*  project name */}
+              <div className="text-[20px] 2xl:text-[24px] text-[#CCCCCC] text-center">
+                {item.name}
+              </div>
+            </Div>
+          );
+        })}
+      </div>
+      {/* PROJECTS GRID START */}
+    </div>
+  );
 };
 
 export default Portfolio;
--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
-import p1 from "./assets/project-1.png";
-import p2 from "./assets/project-2.png";
-import p3 from "./assets/project-3.png";
-import p4 from "./assets/project-4.png";
-import p5 from "./assets/project-5.png";
-import p6 from "./assets/project-6.png";
+import p1 from "./assets/project1.jpeg";
+import p2 from "./assets/project10.jpeg";
+import p3 from "./assets/project3.jpeg";
+import p4 from "./assets/project4.jpeg";
+import p5 from "./assets/project5.jpeg";
+import p6 from "./assets/project6.jpeg";
+import p7 from "./assets/project7.jpeg";
+import p8 from "./assets/project8.jpeg";
+import p9 from "./assets/project9.jpeg";
 
 export const filters = [
     {
@@ -26,34 +29,51 @@ export const filters = [
 
 export const projects = [
     {
-        name: "Join the Innovative Car Wash Service",
-        image: p1,
+        name    : "Web developer personal portfolio using only basic HTML & CSS",
+        image   : p1,
+        live_url: "https://github.com/",
+        github_url:'https://github.com/',
         tags: ["dashboard", "mobile-app"],
     },
     {
-        name: "Your Luggage is in Safe Hands",
+        name: "Movie searching App using react,redux,tailwind and TMDB API",
         image: p2,
         tags: ["dashboard", "website"],
     },
     {
-        name: "The Art of Hiring",
+        name: "Web Developer Portfolio using HTML,CSS & Javascript",
         image: p3,
         tags: ["mobile-app"],
     },
     {
-        name: "We Are Solving Global Problems",
+        name: "Netflix homepage clone using basic HTML & CSS",
         image: p4,
         tags: ["mobile-app"],
     },
     {
-        name: "The English Tea Ceremony",
+        name: "Starbucks homepage clone using basic HTML & CSS",
         image: p5,
         tags: ["website", "dashboard"],
     },
     {
-        name: "Saving Photo Memories",
+        name : "Simple Calculator using Javascript",
         image: p6,
-        tags: ["website"],
+        tags : ["website"],
+    },
+    {
+        name : "DictoGuru- Dictionary App using react, materialUI and google dictionary App",
+        image: p7,
+        tags : ["website"],
+    },
+    {
+        name : "Simple Joke generator using javascript and Joke API",
+        image: p8,
+        tags : ["website"],
+    },
+    {
+        name : "Simple Word Editor using Javascript ",
+        image: p9,
+        tags : ["website"],
     },
 ];
 
